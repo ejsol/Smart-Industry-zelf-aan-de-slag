442a2a86a141228ac6ead7a17a2628b48aa339b2
ejsol <ejsol@dse.nl>
2019-07-17 10:40:35 +0200

    OPC_UA client program for on the RevPi Core3 (rev-3)

---
+++ RevPi/Rev-demo-7-OPC-client-on-own-pc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# (c) EJSol 8 jun 2019 freeware for use in Smart Industry - Zelf Aan de Slag workshop (SIZAS)
+
+from opcua import Client
+import time
+
+url = "opc.tcp://10.0.0.3:4840"
+
+client = Client(url)
+
+client.connect()
+print("Client is connected")
+print(" ")
+print(" log file with data for database storage")
+
+print("                               ")
+print("time stamp                trigger  warehouse-state outside-door  inside-door ")
+
+
+while True:
+    try:
+        time_stamp = client.get_node("ns=2;i=2")
+        trigger = client.get_node("ns=2;i=3")
+        warehouse_state = client.get_node("ns=2;i=4")
+        door_outside = client.get_node("ns=2;i=5")
+        door_inside = client.get_node("ns=2;i=6")
+
+        print(time_stamp.get_value(), "  ", trigger.get_value(), "  ", warehouse_state.get_value(), "          ",
+            door_outside.get_value(), "         ", door_inside.get_value(), " ")
+        time.sleep(5)
+    except KeyboardInterrupt:
+        client.disconnect()
+        exit(1)
